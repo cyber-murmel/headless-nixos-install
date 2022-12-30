944388b30f60b6b75751cdcb0c25f6f76c75e061
cyber-murmel <marble@chaoscomputer.club>
2022-12-30 20:11:09 +0100

    exposed pinned nixos

--- default.nix
+++ default.nix
@@ -1,15 +1,15 @@
-{ pkgs ? import
+{ pkgs_src ?
     (builtins.fetchGit {
       name = "nixos-22.05-2022_08_27";
       url = "https://github.com/nixos/nixpkgs/";
       ref = "refs/heads/nixos-22.05";
       rev = "f11e12ac6af528c1ba12426ce83cee26f21ceafd";
     })
-    { }
 }:
 
 let
-  nixos = import <nixpkgs/nixos> {
+  pkgs = ((import pkgs_src) {});
+  nixos = import "${pkgs_src}/nixos" {
     configuration = { ... }: {
       imports = [
         ./image.nix
